1907c0c461c05037ca2f526fd6e8c8f420dbb64c
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-21 02:39:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function theBeatlesPlay (musicians, instruments) {
   var array = [];
-  
+  for (var i = 0; i < musicians.length; i++) {
+    array.push('${musicians[i]} plays ${instruments[i]}')
+  }
+  return array
 }
